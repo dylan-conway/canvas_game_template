78a69c6702d4a0d96fd3f79b6e6132b74f24fd6f
Dylan Conway <dylan.conway567@gmail.com>
2019-05-28 11:26:04 -0700

    added file imports and start of canvas class

---
+++ src/js/Canvas.js
@@ -0,0 +1,8 @@
+export default class Canvas{
+    constructor(element, context, width, height){
+        this.element = element;
+        this.ctx = context;
+        this.width = width;
+        this.height = height;
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,22 @@
 
+// Import canvas class.
+import Canvas from '../js/Canvas.js';
+
+// Add favicon.
+import faviconImg from '../images/favicon.ico';
+(()=>{
+    let link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = faviconImg;
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
 
 window.onload = o => {
     let check = o;
-    console.log(check);
+    
+    // Get canvas tag and context;
+    let canvasElem = document.getElementById('canvas');
+    let context = canvasElem.getContext('2d');
+    let c = new Canvas(canvasElem, context, innerWidth, innerHeight);
 }
